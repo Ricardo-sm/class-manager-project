acd318d8f2fcdb31f3572640f894e64a6128440c
Ricardo-sm <ricardo.hdz.sm@hotmail.com>
2020-04-18 14:58:54 -0500

    Created database file

---
+++ database/db.sql
@@ -0,0 +1,29 @@
+--Database
+CREATE DATABASE classroom;
+USE classroom;
+
+--Tables
+CREATE TABLE `teacher` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `mail` varchar(60) NOT NULL,
+  `pass` varchar(60) NOT NULL
+);
+
+CREATE TABLE `class` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `class_name` int(11) NOT NULL,
+  `id_teacher` int(11) NOT NULL,
+  CONSTRAINT `class_ifbk_1` FOREIGN KEY(`id_teacher`) REFERENCES teacher(id)
+);
+
+CREATE TABLE `students` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `mark` tinyint(2) NOT NULL,
+  `id_class` int(11) NOT NULL,
+  CONSTRAINT `students_ifbk_1` FOREIGN KEY(`id_class`) REFERENCES class(id)
+);
+
